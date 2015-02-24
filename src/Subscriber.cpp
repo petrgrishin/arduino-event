76a1909ad48a73a02a14ec8296c576f59aad9b12
Petr Grishin <petr.grishin@grishini.ru>
2015-02-25 00:47:48 +0300

    Реализация классов для работы с событиями

--- src/Subscriber.cpp
+++ src/Subscriber.cpp
@@ -1,3 +1,16 @@
 class Subscriber {
+public:
+    void takeEvent(Event event) {
+        if (identity(event)) {
+            process(event);
+        }
+    }
 
+    virtual void process(Event event) {
+
+    }
+
+    virtual bool identity(Event event) {
+        return true;
+    }
 };
