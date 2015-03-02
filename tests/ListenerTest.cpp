e8bc68dce92c58edf8a55b2bdec743271c065dcf
Petr Grishin <petr.grishin@grishini.ru>
2015-03-02 23:57:29 +0300

    Реализация тестов

--- tests/ListenerTest.cpp
+++ tests/ListenerTest.cpp
@@ -9,6 +9,7 @@ class TestSubscriber: public Subscriber {
 };
 
 MU_TEST(TestSubscribeAndTrigger) {
+    i = 0;
     Listener *listener = new Listener();
     Subscriber *subscriber = new TestSubscriber();
     mu_check(i == 0);
@@ -22,8 +23,24 @@ MU_TEST(TestSubscribeAndTrigger) {
     mu_check(i == 4);
 }
 
+MU_TEST(TestVariousSubscribeAndTrigger) {
+    i = 0;
+    Listener *listener = new Listener();
+    Subscriber *subscriber = new TestSubscriber();
+    mu_check(i == 0);
+    listener->on("test1", subscriber)
+            .trigger("test1");
+    mu_check(i == 1);
+    listener->trigger("test2");
+    mu_check(i == 1);
+    listener->on("test2", subscriber);
+    listener->trigger("test2");
+    mu_check(i == 2);
+}
+
 MU_TEST_SUITE(Test) {
     MU_RUN_TEST(TestSubscribeAndTrigger);
+    MU_RUN_TEST(TestVariousSubscribeAndTrigger);
 }
 
 int main(int argc, char *argv[]) {
